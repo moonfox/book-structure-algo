6fe76e820762640fa5ba10709ae30c974922e891
moonfox <sleepmoon.jia@gmail.com>
2019-05-30 23:35:22 +0800

    add gitignore and code

--- ch11/insertion-sort.c
+++ ch11/insertion-sort.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+void insertion_sort(int *ary, int n);
+void print_ary(int ary[], int n);
 
 int main()
 {
@@ -6,15 +8,78 @@ int main()
 
   int n = 6;
 
-  for (size_t i = 1; i < n; i++)
-  {
-    int value = ary[i];
-    int j = i - 1;
+  printf("排序前: ");
+  print_ary(ary, n);
 
-    for (; j < count; i++)
+  insertion_sort(ary, n);
+
+  printf("排序后: ");
+  print_ary(ary, n);
+}
+
+// 算法思路
+// 待插入的元素依次和已经排好的序列中的元素从后向前进行比较
+// 大于要插入的数据时，向后移动一个元素，
+// 当找到位置后(当ary[j] < value时)，插入，
+// 否则和一下个元素再比较
+
+// 程序中，j的范围为有序元素的范围，从0开始
+
+// 要插入元素的位置，要么在已经排序的某个元素的后面a[j + 1]，
+// 要么在已经排序列的第一个位置: a[-1 + 1]
+
+// 当比已经排序序列的第一个还小时，可以看成和一下个要比，时此
+// 因为已经没有要比的了，可以假设前面还有一个a[-1]，但比要插入的元素小
+// 插入位置在它后面，即a[0],a[-1+1]
+// a[-1], a[0], a[1], a[2]....a[5]
+
+void insertion_sort(int *ary, int n)
+{
+
+  int value, j;
+  for (int i = 1; i < n; i++)
+  {
+    // 要插入的元素的值暂存在value中
+    value = ary[i];
+    j = i - 1;
+
+    // 查找插入的位置，并移动数据
+    for (; j >= 0; j--)
     {
+      // 当 ary[j] > value（要插入的元素)，a[j]要向后移动，空出自己的位置
+      // 这个位置就是value要插入的位置，当第一次循环时，a[j]向后移动，覆盖的
+      // 是要插入元素的位置，利用要插入元素的位置取得空位
+
+      // 循环退出的条件是：
+      // 1.要插入的元素比当前比较的元素大
+      // 2.要插入的元素与有序序列中的所有元素比较完成，这种情况是要插入的元素
+      //   被放在了有序序列的第一个位置(比所有元素都小)
+
+      if (ary[j] > value)
+      {
+        // 注意:这里执行 j--
+        ary[j + 1] = ary[j];
+      }
+      else
+        // 注意:这里不执行 j--
+        break;
     }
 
-    j = i;
+    // 插入数据: 当ary[j] < value时
+    // 当ary[j] < value时，value插入的位置为j的后面，即[j + 1]，此时不用再移动数据
+    // 当value比所有要比的都小时，value要插入的位置为a[0],此时j = -1
+    // ary[j + 1] 刚好为ary[0]
+
+    // ary[j + 1] = value,j + 1为空出来的位置，之所以有空位是因为元素向后移动，占用了
+    // 要插入元素的位置，要插入的元素保存在value中
+    ary[j + 1] = value;
   }
+}
+
+// 打印数组
+void print_ary(int ary[], int n)
+{
+  for (int i = 0; i < n; i++)
+    printf("%d ", ary[i]);
+  printf("\n");
 }
